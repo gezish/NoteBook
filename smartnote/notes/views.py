24f2c4f193cb24ca624c8a86b18d54fe476a6483
gezish <enggezahegn.w@gmail.com>
2024-01-11 15:01:20 +0300

    function based configuration done

--- smartnote/notes/views.py
+++ smartnote/notes/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
-
+from django.http import Http404
 from .models import Notes
 
 
 def list(request):
     all_notes = Notes.objects.all()
     return render(request, 'notes/notes_list.html',{'notes': all_notes})# get all notes and send them to a templete
+
+
+def detail(request,pk):
+    try:
+        note = Notes.objects.get(pk=pk)
+    except Notes.DoesNotExist:
+        raise Http404("Note does not exist")    
+        
+    return render(request, 'notes/notes_detail.html',{'note': note})
+    
